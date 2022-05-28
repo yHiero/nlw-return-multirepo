da033d13764483959eb7ea599f3d4b6470372152
yHiero <herik.silva.hs@gmail.com>
2022-05-28 15:49:03 -0300

    APP mobile, web e server desenvolvido junto com a rocketseat

---
+++ server/src/adapters/nodemailer/nodenailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a34878abf39334",
+    pass: "1017ef8383458b"
+  }
+})
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Herik Silva <herik.silva.sh@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+// GET, POST PUT, PATCH, DELETE
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar uma informações única de uma entidade
+// DELETE = Deletar uma info
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333, () => {
+  console.log('HTTP server running');
+});
